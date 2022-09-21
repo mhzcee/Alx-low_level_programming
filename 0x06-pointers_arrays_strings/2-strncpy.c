2af90c7a723798c42b2eb182c44deb105ee75655
mhzcee <cjaykeyz@gmail.com>
2022-09-21 14:05:27 -0700

    a function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strncpy - Copy a string starting from index 0 of `dest`.
+ * _strncpy - Copy a string starting from index 0 of `dest`
  * @dest: string
  * @src: string
  * @n: number of chars to copy over
  * Return: `dest` edited string
- */
+*/
 
 char *_strncpy(char *dest, char *src, int n)
 {
